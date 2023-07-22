fba103f2d9ecd1766861e744f3281a711b0571a4
l1nes8 <egor1glavatyje@gmail.com>
2023-07-22 13:57:21 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+function renderGallery() {
+  const createEl = galleryItems
+    .map(
+      (item) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/>
+  </a>
+</li>`,
+    )
+    .join('');
+
+  galleryContainer.innerHTML = createEl;
+}
+renderGallery();
+
+galleryContainer.addEventListener('click', mouseClickGallery);
+
+function mouseClickGallery(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const largeImageUrl = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${largeImageUrl}" alt="Image description">
+    `);
+
+    instance.show();
+
+    document.addEventListener('keydown', onKeyPressClose);
+
+    function onKeyPressClose(event) {
+      if (event.code === 'Escape') {
+        instance.close();
+
+        document.removeEventListener('keydown', onKeyPressClose);
+      }
+    }
+  }
+}
+
 console.log(galleryItems);
